63ef85a0249895de6be98cf5bd09c6751645426b
Malik Umer Farooq <Lablnet01@gmail.com>
2017-06-11 23:24:17 -0700

    Update search.php

--- plugins/default/css/search.php
+++ plugins/default/css/search.php
@@ -4,10 +4,9 @@
 .ossn-search input[type='text']{
     width: 70%;
     margin: 0 auto;
-    background-color:#73087f;
     color: #ffffff;
     margin-top: 10px;
     padding: 3px;
-        padding-left: 6px;
+    padding-left: 6px;
     border-radius: 3px;
 }
